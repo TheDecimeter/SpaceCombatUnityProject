c9de04af1a906bc059670f34c232508684bee37f
Daniel Williams <spawnofthepilgrims@googlemail.com>
2019-02-21 12:28:44 -0700

    NUKED

--- Prototype Platformer/Assets/Class Prototype/Scripts/FriendlyInputScripts/ControlStruct.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ControlStruct
-{
-    public bool jump=false, attack=false, action=false;
-    public float moveLeft=0;
-}
