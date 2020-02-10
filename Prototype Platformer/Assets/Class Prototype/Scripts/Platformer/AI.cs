4028319d45fd64884e3a6b1674fb90a5a8d225f5
Daniel Williams <spawnofthepilgrims@googlemail.com>
2020-02-10 16:55:31 -0700

    Added mapping of a tile for AI
    Still needs to be mapped from a central point.
    Stairs need to be an obvious obsticle

--- Prototype Platformer/Assets/Class Prototype/Scripts/Platformer/AI.cs
+++ Prototype Platformer/Assets/Class Prototype/Scripts/Platformer/AI.cs
@@ -12,7 +12,8 @@ public class AI : MonoBehaviour
     Rigidbody rb;
     private float roomCell;
 
-    private bool [][] path;
+    private bool [][] levelPath;
+    private int[][] roomPath;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -21,10 +22,15 @@ public class AI : MonoBehaviour
         StartX = levelStats.startX;
         roomCell = levelStats.yTileSize / 2;
 
-        path = new bool[levelStats.MapDemensionsY][];
+        levelPath = new bool[levelStats.MapDemensionsY][];
         for (int i = 0; i < levelStats.MapDemensionsY; ++i)
         {
-            path[i] = new bool[levelStats.MapDemensionsX];
+            levelPath[i] = new bool[levelStats.MapDemensionsX];
+        }
+        roomPath = new int[2][];
+        for (int i = 0; i < 2; ++i)
+        {
+            roomPath[i] = new int[3];
         }
     }
 
@@ -34,12 +40,85 @@ public class AI : MonoBehaviour
         //print(FindNearestExit());
         int x;
         int y;
-        GetRoomGrid(transform.position, out x, out y);
+        //GetRoomGrid(transform.position, out x, out y);
+        MapRoom();
     }
 
     private void SetLevelPath()
     {
+        
+    }
 
+    private void MapRoom()
+    {
+        ResetRoomPath();
+        int gridx; int gridy;
+        GetGridPos(transform.position,out gridx, out gridy);
+        int roomx; int roomy;
+        GetRoomGrid(transform.position, out roomx, out roomy);
+
+        Queue<MapNode> n = new Queue<MapNode>();
+
+        n.Enqueue(new MapNode(roomx, roomy, 1, transform.position));
+
+        RaycastHit h;
+
+        while (n.Count > 0)
+        {
+            MapNode c = n.Dequeue();
+
+            if (roomPath[c.y][c.x] != 0)
+                continue;
+            //print("at " + c.x + "," + c.y);
+            roomPath[c.y][c.x] = c.cost;
+
+            //Debug.DrawRay(c.loc, Vector3.left * roomCell * .8f);
+            //Debug.DrawRay(c.loc, Vector3.right * roomCell * .8f, Color.red);
+            //Debug.DrawRay(c.loc, Vector3.up * roomCell * 1.5f, Color.gray);
+            //Debug.DrawRay(c.loc, Vector3.down * roomCell * 5f, Color.blue);
+            //check left
+            if (!(c.x - 1 < 0 || c.x - 1 > 2 || c.y < 0 || c.y > 1 || roomPath[c.y][c.x - 1] != 0)&&
+                !Physics.Raycast(c.loc, Vector3.left, out h, roomCell * .8f))
+                n.Enqueue(new MapNode(c.x - 1, c.y, c.cost + 1, new Vector3(c.loc.x - roomCell, c.loc.y, c.loc.z)));
+            //check right
+            if (!(c.x + 1 < 0 || c.x + 1 > 2 || c.y < 0 || c.y > 1 || roomPath[c.y][c.x + 1] != 0) &&
+                !Physics.Raycast(c.loc, Vector3.right, out h, roomCell * .8f))
+                n.Enqueue(new MapNode(c.x + 1, c.y, c.cost + 1, new Vector3(c.loc.x + roomCell, c.loc.y, c.loc.z)));
+            //check up
+            if (!(c.x < 0 || c.x > 2 || c.y + 1 < 0 || c.y + 1 > 1 || roomPath[c.y + 1][c.x] != 0) &&
+                !Physics.Raycast(c.loc, Vector3.up, out h, roomCell * 1.5f))
+                n.Enqueue(new MapNode(c.x, c.y + 1, c.cost + 1, new Vector3(c.loc.x, c.loc.y + roomCell, c.loc.z)));
+            //check down
+            if (!(c.x < 0 || c.x > 2 || c.y - 1 < 0 || c.y - 1 > 1 || roomPath[c.y - 1][c.x] != 0) &&
+                !Physics.Raycast(c.loc, Vector3.down, out h, roomCell*.5f ))
+                n.Enqueue(new MapNode(c.x, c.y - 1, c.cost + 1, new Vector3(c.loc.x, c.loc.y - roomCell, c.loc.z)));
+        }
+
+        //string s = "\n";
+
+        //for (int j = 0; j < 3; ++j)
+        //{
+        //    s += roomPath[1][j];
+        //}
+        //s += "\n";
+        //for (int j = 0; j < 3; ++j)
+        //{
+        //    s += roomPath[0][j];
+        //}
+
+        //print(s);
+
+    }
+
+    private void ResetRoomPath()
+    {
+        for (int i = 0; i < 2; ++i)
+        {
+            for (int j = 0; j < 3; ++j)
+            {
+                roomPath[i][j] = 0;
+            }
+        }
     }
 
 
@@ -99,9 +178,10 @@ public class AI : MonoBehaviour
         int gridX;
         int gridY;
         GetGridPos(loc, out gridX, out gridY);
-        x = (int)((loc.x - StartX-levelStats.xTileSize/6-gridX*levelStats.xTileSize) / 3);
-        y = (int)((loc.y - StartY-gridY * levelStats.yTileSize) / 2);
-        //print("room grid " + x + "," + y);
+        TileInformation room = levelStats.Map[gridY][gridX];
+        Vector3 roomLoc = room.transform.position;
+        x = (int)((loc.x - roomLoc.x) / 4);
+        y = (int)((loc.y - (roomLoc.y+StartY)) / 3);
         x = Mathf.Clamp(x, 0, 2);
         y = Mathf.Clamp(y, 0, 1);
 
@@ -221,5 +301,22 @@ public class AI : MonoBehaviour
         }
 
         return closest;
+    }
+
+    private struct MapNode
+    {
+        public int x;
+        public int y;
+        public int cost;
+        public Vector3 loc;
+
+        public MapNode(int x, int y, int cost, Vector3 loc) {
+            //print("created Node (" + x + "," + y + ") " + cost + " " + loc);
+            this.x = x;
+            this.y = y;
+            this.cost = cost;
+            this.loc = loc;
+        }
+
     }
 }
