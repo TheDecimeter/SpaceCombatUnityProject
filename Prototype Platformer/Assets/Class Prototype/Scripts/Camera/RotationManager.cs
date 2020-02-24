3850afc2190c39f3851db1e0ac5c07775631a3f5
Daniel Williams <spawnofthepilgrims@googlemail.com>
2020-02-24 10:52:03 -0700

    Added rotation idea

---
+++ Prototype Platformer/Assets/Class Prototype/Scripts/Camera/RotationManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationManager : MonoBehaviour
+{
+    public Transform ObjectToRotate;
+    public ControlLayouts [] MobileControlLayouts;
+    
+    [System.Serializable]
+    public struct ControlLayouts
+    {
+        public GameObject Controls;
+        public float Rotation;
+    }
+
+    private int currentRot = 0;
+    
+
+    public void Rotate()
+    {
+        MobileControlLayouts[currentRot].Controls.SetActive(false);
+        currentRot++;
+        if (currentRot == MobileControlLayouts.Length)
+            currentRot = 0;
+
+        MobileControlLayouts[currentRot].Controls.SetActive(true);
+
+        ObjectToRotate.rotation = Quaternion.Euler(0, 0, MobileControlLayouts[currentRot].Rotation);
+    }
+}
