6b23abb015dfca3c776bf5f2f037139c01ae9053
Daniel Williams <spawnofthepilgrims+git@googlemail.com>
2020-03-12 21:25:44 -0600

    Added some fonts, cropped title image to only show title (no instructions)

---
+++ Prototype Platformer/Assets/Class Prototype/Scripts/Utility/TEST/SetText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SetText : MonoBehaviour
+{
+    public string text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<TextMeshProUGUI>().text = text;
+    }
+    
+}
