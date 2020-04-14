be2c4982d87adfe763e99d17570f4a76a45ee006
Daniel Williams <spawnofthepilgrims+git@googlemail.com>
2020-04-14 01:22:04 -0600

    Added mouse over effect for links

--- Prototype Platformer/Assets/Class Prototype/Scripts/Utility/GoToLink.cs
+++ Prototype Platformer/Assets/Class Prototype/Scripts/Utility/GoToLink.cs
@@ -2,19 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using TMPro;
 
-public class GoToLink : MonoBehaviour, IPointerClickHandler
+public class GoToLink : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public string Link;
+    TextMeshProUGUI text;
+    private Color hover,normal;
 
     private void Start()
     {
-
+        text = GetComponent<TextMeshProUGUI>();
+        if (text)
+        {
+            if(text.color.a>.5f)
+                hover = new Color(text.color.r, text.color.g, text.color.b, text.color.a / 2);
+            else
+                hover = new Color(text.color.r, text.color.g, text.color.b, 1);
+            normal = new Color(text.color.r, text.color.g, text.color.b, text.color.a);
+        }
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
         print("open url " + Link);
         Application.OpenURL(Link);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if(text)
+            text.color = hover;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if(text)
+            text.color = normal;
     }
 }
